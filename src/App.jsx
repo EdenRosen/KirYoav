bf9f9240d224a86393d638429c0ed374f79c4c11
EdenRosen <edenrosen500@gmail.com>
2024-08-15 23:59:24 +0300

    night

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,8 @@ function App() {
     <div className="App">
       <div className="content">
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/climbers" element={<Climbers/>} />
+          <Route path="/KirYoav/" element={<Home/>} />
+          <Route path="/KirYoav/climbers" element={<Climbers/>} />
           
           {/* { !currentUser && <Route path="/login" element={<Login/>} /> }
           { !currentUser && <Route path="/signup" element={<Signup/>} /> }
