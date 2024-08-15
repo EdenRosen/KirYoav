1bf38f65b73ca0a2fb3d299e01ed2d3397e8637e
EdenRosen <edenrosen500@gmail.com>
2024-08-15 18:04:52 +0300

    second

--- server/utils/generateId.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto')
-
-function generateId() {
-    const digits = 8
-    const id = crypto.randomInt(10**digits, 10**(digits+1) - 1)
-    return id
-}
-
-module.exports = generateId
